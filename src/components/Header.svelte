ee5f1d7fdc4f226e82d4a6aaa356ca047ad3fd64
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-14 17:32:55 -0600

    first commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let y;
+
+  let tabs = [
+    { name: "Projects", link: "#projects" },
+    { name: "About me", link: "#about" },
+  ];
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + (
+    y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
+)}>
+  <h1 class="font-medium">
+    <b class="font-bold poppins">David</b> Vargas
+    <div class="sm:flex items-center gap-4 hidden">
+      {#each tabs as tab, index}
+        <a href="{tab.link}" class="duration-200 hover:text-violet-400">
+          <p>{tab.name}</p>
+        </a>
+      {/each}
+    </div>
+  </h1>
+</header>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#04002d',
+      }
+    }
+  },
+  plugins: []
+};
